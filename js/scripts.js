476192e3e122a426cb758d348149466832dc483f
Kody Gordon <gordon.km89@gmail.com>
2021-01-29 08:37:03 -0800

    Working on business logic

--- js/scripts.js
+++ js/scripts.js
@@ -13,17 +13,38 @@ function reciept(size, cost, toppings) {
   this.cost = cost; 
   this.toppings = toppings;
 }
+function size(small, medium, large, family) {
+  this.small = small;
+  this.medium = medium;
+  this.large = large;
+  this.family = family;
+}
+function toppings(pepperoni, cheese, bacon, olives, sausage, jalapenos) {
+  this.pepperoni = pepperoni;
+  this.cheese = cheese;
+  this.bacon = bacon;
+  this.olives = olives;
+  this.sausage = sausage;
+  this.jalapenos = jalapenos;
+} 
 
+reciept.prototype.toppings = function() {
+  this.toppings = this.size + toppings;
+  if ()
+}
 
-reciept.prototype.cost = function() {
+reciept.prototype.pizzaCost = function() {
   this.price = 5.00;
-  if (this.time === "small") {
+  if (this.small === "small") {
     this.price += 3.00;
   }
-  if (this.age === "medium") {
-    this.price += 4.00;
+  if (this.medium === "medium") {
+    this.price += 6.00;
   }
-  if (this.newMovie === "large") {
-    this.price += 5.00;
+  if (this.large === "large") {
+    this.price += 9.00;
+  }
+  if (this.family === "family") {
+    this.price += 12.00;
   }
 };
