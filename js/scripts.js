7558e24824e53eacc3d313d4c5132d2d65e6051d
Kody Gordon <gordon.km89@gmail.com>
2021-01-29 10:09:28 -0800

    Added for business logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@ function Pizzaria() {
   this.currendId = 0;
 }
 
-Pizzaria.prototype.assignId = function() {
+Pizzaria.prototype.assignId = function () {
   this.currentId += 1;
   return this.currentId; // remove later
 }
 
 // Business logic for pizzaria
 function reciept(size, cost, toppings) {
-  this.size = size; 
-  this.cost = cost; 
+  this.size = size;
   this.toppings = toppings;
+  this.cost = cost;
 }
 function size(small, medium, large, family) {
   this.small = small;
@@ -19,7 +19,7 @@ function size(small, medium, large, family) {
   this.large = large;
   this.family = family;
 }
-function toppings(pepperoni, cheese, bacon, olives, sausage, jalapenos) {
+function toppings(sausage, pepperoni, mushrooms, onions, olives, jalapenos, bacon) {
   this.sausage = sausage;
   this.pepperoni = pepperoni;
   this.mushrooms = mushrooms;
@@ -27,38 +27,45 @@ function toppings(pepperoni, cheese, bacon, olives, sausage, jalapenos) {
   this.olives = olives;
   this.jalapenos = jalapenos;
   this.bacon = bacon;
-} 
+  this.toppingTotal = sausage + pepperoni + mushrooms + onions + olives + jalapenos + bacon;
+}
 
-reciept.prototype.toppings = function() {
-  this.price = 5.00;
+reciept.prototype.toppings = function () {
+  this.price = this.size + toppingTotal;
+  if (this.sausage === "sausage") {
+    this.price += 1.00;
+  }
   if (this.pepperoni === "pepperoni") {
     this.price += 1.00;
   }
-  if (this.bacon === "bacon") {
+  if (this.mushrooms === "mushrooms") {
+    this.price += 1.00;
+  }
+  if (this.onions === "onions") {
     this.price += 1.00;
   }
   if (this.olives === "olives") {
     this.price += 1.00;
   }
-  if (this.sausage === "sausage") {
-    this.price += 1.00;
-  }
   if (this.jalapenos === "jalapenos") {
     this.price += 1.00;
-};
+  }
+  if (this.bacon === "bacon") {
+    this.price += 1.00;
+  }
 
-reciept.prototype.size = function() {
-  this.price = 5.00;
-  if (this.small === "small") {
-    this.price += 3.00;
-  }
-  if (this.medium === "medium") {
-    this.price += 6.00;
-  }
-  if (this.large === "large") {
-    this.price += 9.00;
-  }
-  if (this.family === "family") {
-    this.price += 12.00;
-  }
-};
+  reciept.prototype.size = function () {
+    this.price = 5.00;
+    if (this.small === "small") {
+      this.price += 3.00;
+    }
+    if (this.medium === "medium") {
+      this.price += 6.00;
+    }
+    if (this.large === "large") {
+      this.price += 9.00;
+    }
+    if (this.family === "family") {
+      this.price += 12.00;
+    }
+  };
